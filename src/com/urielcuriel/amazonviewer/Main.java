0fe27ef2066aae4e49da3e2adae30114f412a0f9
Uriel <sky_dregon_16@hotmail.com>
2018-06-20 03:45:03 -0500

    create model package

--- src/com/urielcuriel/amazonviewer/Main.java
+++ src/com/urielcuriel/amazonviewer/Main.java
@@ -1,27 +1,49 @@
 package com.urielcuriel.amazonviewer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int exit = 0;
+		//reader from command line input
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean exit = false;
 		do {
-			
-			System.out.println("BIENVENIDOS AMAZON VIEWER");
+			System.out.println("BIENVENIDOS A AMAZON VIEWER");
 			System.out.println("");
-			System.out.println("Selecciona el nÃºmero de la opciÃ³n deseada");
+			System.out.println("Selecciona el número de la opción deseada");
 			System.out.println("1. Movies");
 			System.out.println("2. Series");
 			System.out.println("3. Books");
 			System.out.println("4. Magazines");
 			System.out.println("0. Exit");
-			
-			//Leer la respuesta del usuario
-			
-			
-			
-			
-		}while(exit != 0);
+			int option = Byte.parseByte(br.readLine());
+			System.out.println(option);
+			switch (option) {
+			case 0:
+				exit = true;
+				break;
+			case 1:
+				System.out.println("Movies");
+				break;
+			case 2:
+				System.out.println("Series");
+				break;
+			case 3:
+				System.out.println("Books");
+				break;
+			case 4:
+				System.out.println("Magazines");
+				break;
+
+			default:
+				System.out.println("La opción "+option+" no existe");
+				break;
+			}
+		} while (!exit);
 		
 	}
 
---
+++ src/com/urielcuriel/amazonviewer/model/Movie.java
@@ -0,0 +1,5 @@
+package com.urielcuriel.amazonviewer.model;
+
+public class Movie {
+
+}
